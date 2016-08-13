f6d8a6acecccd338c2a586b2bc1a36a5d179b4f8
Tommy Muehle <tommy.muehle@gmail.com>
2016-08-13 13:24:31 +0200

    Add tests for download helper

---
+++ tests/Script/Helper/DownloaderTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tooly\Tests\Script\Helper;
+
+use phpmock\phpunit\PHPMock;
+use Tooly\Script\Helper\Downloader;
+
+/**
+ * @package Tooly\Tests\Script\Helper
+ */
+class DownloaderTest extends \PHPUnit_Framework_TestCase
+{
+    use PHPMock;
+
+    public function testAccessibleTestWorksCorrect()
+    {
+        $downloader = new Downloader;
+
+        $this->assertFalse($downloader->isAccessible('foo'));
+        $this->assertTrue($downloader->isAccessible('https://github.com/tommy-muehle/tooly-composer-script/blob/master/README.md'));
+    }
+
+    public function testCanDownloadContentFromUrl()
+    {
+        $downloader = new Downloader;
+
+        $this->assertRegExp(
+            '/tooly-composer-script/',
+            $downloader->download('https://github.com/tommy-muehle/tooly-composer-script/blob/master/README.md')
+        );
+    }
+}
